2e93a0602da1a2c0783125dea1976122c079908d
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-09-18 22:24:36 +0630

    added server util

---
+++ src/main/java/com/_p1m/portfolio/common/util/ServerUtil.java
@@ -0,0 +1,11 @@
+package com._p1m.portfolio.common.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+
+public class ServerUtil {
+
+}
