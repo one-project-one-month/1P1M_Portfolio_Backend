414c56ba2902dea397f64bf9a11224d0f84eb565
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-09-18 13:31:50 +0630

    added folders

---
+++ 1P1M_Portfolio/src/test/java/com/project/_P1M_Portfolio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project._P1M_Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
