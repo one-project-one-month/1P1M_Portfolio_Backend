4d4200307a0023b9b11b123b238027da69e85175
Furry33 <mathet426@gmail.com>
2025-10-11 15:04:25 +0700

    v.11

--- src/main/java/com/_p1m/portfolio/features/projectPortfolio/dto/request/DeleteProjectPortfolioPictureRequest.java
+++
@@ -1,8 +0,0 @@
-package com._p1m.portfolio.features.projectPortfolio.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DeleteProjectPortfolioPictureRequest(
-		@NotBlank(message = "ImageUrl cannot be empty or null.")
-		String imageUrl
-) {}
--- src/main/java/com/_p1m/portfolio/features/projectPortfolio/dto/response/AssignedDevs.java
+++
@@ -1,5 +0,0 @@
-package com._p1m.portfolio.features.projectPortfolio.dto.response;
-
-import java.util.List;
-
-public record AssignedDevs(List<Long> devProfileIds) {};
