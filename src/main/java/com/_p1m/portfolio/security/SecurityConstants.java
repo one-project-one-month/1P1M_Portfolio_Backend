25bbd8bd92afcd914a1c19e9709675c40b4de57c
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-10-14 22:40:28 +0630

    [FIXED] Security Constants

--- src/main/java/com/_p1m/portfolio/security/SecurityConstants.java
+++ src/main/java/com/_p1m/portfolio/security/SecurityConstants.java
@@ -5,8 +5,7 @@ public class SecurityConstants {
     public static final String[] WHITELIST = {
             // Authentication & OAuth
             "/portfolio/api/v1/auth/users/**",
-            "/portfolio/api/v1/**",
-            "http://localhost:8080/portfolio/api/v1/register",
+
             // Swagger & API Docs
             "/swagger-ui/**",
             "/v3/api-docs/**",
