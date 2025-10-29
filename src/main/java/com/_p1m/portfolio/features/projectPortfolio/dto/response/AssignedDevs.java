908bdfcde57d03b9261676ad24c214e65813a680
hlyamhtetkyaw <hlan1559@gmail.com>
2025-10-29 09:49:16 +0630

    [ADD] profile details

--- src/main/java/com/_p1m/portfolio/features/projectPortfolio/dto/response/AssignedDevs.java
+++ src/main/java/com/_p1m/portfolio/features/projectPortfolio/dto/response/AssignedDevs.java
@@ -2,4 +2,13 @@ package com._p1m.portfolio.features.projectPortfolio.dto.response;
 
 import java.util.List;
 
-public record AssignedDevs(List<Long> devProfileIds) {};
+public record AssignedDevs(List<DevProfileResponse> developers) {
+    public record DevProfileResponse(
+        Long id,
+        String name,
+        String profilePictureUrl,
+        String github,
+        String linkedIn,
+        String aboutDev
+    ) {}
+}
